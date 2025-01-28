4491f15259396bf47be1714ee72087f971041c12
Tyrone Williams <williamsjtyrone@gmail.com>
2025-01-28 16:45:58 -0500

    Add delete and edit modal

--- web/app/routes/dashboard/transactions/TransactionPage.tsx
+++ web/app/routes/dashboard/transactions/TransactionPage.tsx
@@ -10,27 +10,95 @@ import {
 import CogWheel from "~/components/icons/CogWheel";
 import Trash from "~/components/icons/Trash";
 import Edit from "~/components/icons/Edit";
+import ConfirmButton from "~/components/ConfirmButton";
+import { data, Link, useFetcher } from "react-router";
+import { eq } from "drizzle-orm";
+import { Transactions } from "~/lib/schema";
+import { db } from "~/lib/db";
+import { useState } from "react";
+import EditTransaction from "~/components/EditTransaction";
+import { DialogContent, Dialog } from "~/components/ui/dialog";
+import Arrow from "~/components/icons/Arrow";
 
 export async function loader({ params }: Route.LoaderArgs) {
   const { id } = params;
 
-  return {
-    id,
-    title: "test",
-    amount: 123,
-    type: "payment",
+  const transaction = await db.query.Transactions.findFirst({
+    where: eq(Transactions.id, id),
+  });
 
-    date: new Date(),
-  };
+  if (!transaction) throw data(null, { status: 404 });
+
+  return transaction;
+}
+
+function DeleteModal({
+  id,
+  visible,
+  onClose,
+}: {
+  id: string;
+  visible: boolean;
+  onClose: () => void;
+}) {
+  const fetcher = useFetcher();
+
+  return (
+    <Dialog open={visible} onOpenChange={onClose}>
+      <DialogContent>
+        <div>
+          <h3>Confirmation</h3>
+          <p>Are you sure you want to delete this transactions?</p>
+
+          <div className="flex flex-row flex-nowrap justify-between gap-4">
+            <fetcher.Form
+              action={`/api/transactions/${id}/delete`}
+              method="post"
+            >
+              <button type="submit">Confirm</button>
+
+              <button type="button" onClick={onClose}>
+                Cancel
+              </button>
+            </fetcher.Form>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
 }
 
 export default function TransactionPage({ loaderData }: Route.ComponentProps) {
+  const [editing, setEditing] = useState(false);
+  const [deleteModal, setDeleteModal] = useState(false);
+  const fetcher = useFetcher();
   const transaction = loaderData;
+
+  console.log({ editing });
 
   return (
     <div>
+      <Link
+        className="flex flex-row flex-nowrap items-center text-slate-500 py-2 text-sm hover:text-slate-900 stroke-slate-500  hover:stroke-slate-900"
+        to={`/dashboard/accounts/${transaction.accountId}`}
+      >
+        <Arrow className="w-4 h-4 mr-1" />
+        Back
+      </Link>
       <header className="flex flex-row flex-nowrap items-center justify-between">
         <h3 className="font-semibold text-xl">Transaction Detail</h3>
+
+        <EditTransaction
+          transaction={transaction}
+          visible={editing}
+          onClose={() => setEditing(false)}
+        />
+
+        <DeleteModal
+          id={transaction.id}
+          visible={deleteModal}
+          onClose={() => setDeleteModal(false)}
+        />
 
         <DropdownMenu>
           <DropdownMenuTrigger>
@@ -49,13 +117,17 @@ export default function TransactionPage({ loaderData }: Route.ComponentProps) {
             </DropdownMenuItem>
 
             <DropdownMenuItem>
-              <div className="flex flex-row flex-nowrap items-center hover:cursor-pointer hover:bg-slate-400/10 px-2">
+              <button
+                type="button"
+                onClick={() => setEditing((old) => !old)}
+                className="flex flex-row flex-nowrap items-center hover:cursor-pointer hover:bg-slate-400/10 px-2"
+              >
                 <Edit className="w-5 h-5" />
                 <div className="ml-2">Edit</div>
-              </div>
+              </button>
             </DropdownMenuItem>
 
-            <DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setDeleteModal(true)}>
               <div className="flex flex-row flex-nowrap items-center hover:cursor-pointer hover:bg-slate-400/10 px-2">
                 <Trash className="w-5 h-5" />
                 <div className="ml-2">Delete</div>
@@ -87,7 +159,7 @@ export default function TransactionPage({ loaderData }: Route.ComponentProps) {
             <tr>
               <td>Date </td>
               <td>
-                {transaction.date.toLocaleDateString("en-US", {
+                {transaction.createdAt.toLocaleDateString("en-US", {
                   day: "2-digit",
                   year: "numeric",
                   month: "short",
