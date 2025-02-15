c7183691116b0f4321222b5f62d33abde3b56762
Tyrone Williams <williamsjtyrone@gmail.com>
2025-02-14 23:32:58 -0500

    Balance is now updated on tranaction creation

--- web/app/routes/api/transactions/create.tsx
+++ web/app/routes/api/transactions/create.tsx
@@ -1,8 +1,9 @@
+import { eq, sql } from "drizzle-orm";
 import type { Route } from "./+types/create";
 import { getAuth } from "@clerk/react-router/ssr.server";
 import { z } from "zod";
 import { db } from "~/lib/db";
-import { Transactions } from "~/lib/schema";
+import { Transactions, Accounts } from "~/lib/schema";
 
 export async function action(args: Route.ActionArgs) {
   const user = await getAuth(args);
@@ -19,12 +20,18 @@ export async function action(args: Route.ActionArgs) {
 
   if (!result.success) return { status: 400, body: result.error };
 
-  await db.insert(Transactions).values({
-    title: result.data.title,
-    amount: result.data.amount,
-    type: "payment",
-    accountId: result.data.account,
-    userId: user.userId as string,
+  await db.transaction(async (tx) => {
+    await db.insert(Transactions).values({
+      title: result.data.title,
+      amount: result.data.amount,
+      type: "payment",
+      accountId: result.data.account,
+      userId: user.userId as string,
+    }),
+      await db
+        .update(Accounts)
+        .set({ balance: sql`${Accounts.balance} + ${result.data.amount}` })
+        .where(eq(Accounts.id, result.data.account));
   });
 
   return { status: 200 };
